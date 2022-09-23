5f1bf342e2053ceb9f406c7cd2884cd17f9b49fb
unknown <k3nshi9@gmail.com>
2022-09-23 17:24:25 +0600

    adding single product details page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Details from '../components/Product components/Details'
+import Footer from '../components/Footer'
+
+
+
+const Products = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Details/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Products
