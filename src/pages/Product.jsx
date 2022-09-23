40a291014e95b56860efaac54ec95a461c7eb4fa
unknown <k3nshi9@gmail.com>
2022-09-23 17:28:41 +0600

    directory error solved

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Details from '../components/Product components/Details'
+import Details from '../components/Product_Components/Details'
 import Footer from '../components/Footer'
 
 
